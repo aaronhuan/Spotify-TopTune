2e5a2f40ed60b8a3bdeeb201b61541e106edf97b
Aaron Huang <aaronhuang0000@gmail.com>
2024-12-30 21:47:16 -0500

    fixed src for bootstrap js - tested html2canvas, worked - working on the 'download' button

---
+++ static/script.js
@@ -0,0 +1,10 @@
+// const { default: html2canvas } = require("html2canvas");
+
+// document.querySelector('#downloadBtn').addEventListener('click', function(){
+//     html2canvas(document.querySelector("#capture")).then(canvas=> {
+//     document.body.appendChild(canvas)
+//     console.log("complete")
+// })
+// })
+
+
