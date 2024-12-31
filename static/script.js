8eb24eb279fc303f2398c32f51980b62b17b6f75
Aaron Huang <aaronhuang0000@gmail.com>
2024-12-31 11:15:19 -0500

    integrate html2canvas - move to static folder - integrated the canvas so that it renders when the button is clicked - use allow taint to render images - use ignore elements to ignore the button when 'downloading'

--- static/script.js
+++ static/script.js
@@ -1,10 +1,12 @@
-// const { default: html2canvas } = require("html2canvas");
+document.querySelector('#downloadBtn').addEventListener('click', function(){
+    html2canvas(document.querySelector("#capture"), {
+        allowTaint: true,
+        ignoreElements: (element) => element.id === 'downloadBtn'
+    }).then(canvas=> {
+    document.body.appendChild(canvas);
+    console.log("complete");
+})
+})
 
-// document.querySelector('#downloadBtn').addEventListener('click', function(){
-//     html2canvas(document.querySelector("#capture")).then(canvas=> {
-//     document.body.appendChild(canvas)
-//     console.log("complete")
-// })
-// })
 
 
