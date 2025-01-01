9d383c0caacbcbcc775224b28322c9421e2cba9a
Aaron Huang <aaronhuang0000@gmail.com>
2025-01-01 16:39:54 -0500

    download to png -button allows download to png

--- static/script.js
+++ static/script.js
@@ -1,10 +1,14 @@
 document.querySelector('#downloadBtn').addEventListener('click', function(){
     html2canvas(document.querySelector("#capture"), {
         allowTaint: true,
-        ignoreElements: (element) => element.id === 'downloadBtn'
-    }).then(canvas=> {
-    document.body.appendChild(canvas);
-    console.log("complete");
+        useCORS: true,
+        ignoreElements: (element) => element.id === 'downloadBtn',
+        }).then(canvas=> {
+    const downloadLink = document.createElement("a");
+    downloadLink.href=canvas.toDataURL("image/png");
+    downloadLink.download = "screenshot.png"
+    // document.body.appendChild(canvas);
+    downloadLink.click()
 })
 })
 
